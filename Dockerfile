5d45a8398ab91d2aeafd1fcd592173741b91e2d4
TorchCAT <Admin@localhost>
2022-01-30 13:24:57 +0000

    MVP add vrotend

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,19 @@
+FROM node:16-alpine AS builder
+RUN apk add git
+RUN git clone https://github.com/lycanru/za_record
+WORKDIR /za_record
+RUN yarn install --frozen-lockfile
+# COPY . .
+RUN npm run build
+#RUN ./node_modules/next/dist/bin/next export
+RUN npx next export
+
 FROM python:3.8-slim-buster
 WORKDIR /app
 RUN mkdir uploads
 COPY requirements.txt .
 RUN pip3 install -r requirements.txt
 COPY . .
+COPY --from=builder /za_record/out ./pages
 ENV FLASK_APP="main.py"
 CMD ["flask", "run", "--host=0.0.0.0"]
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_from_directory
 from cumspeech import cmspch
 import requests
 
@@ -8,32 +8,41 @@ UPLOAD_FOLDER = 'uploads'
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['JSON_AS_ASCII'] = False
+app.config['PAGES'] = 'pages'
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=["GET"])
+def index():
+    return send_from_directory(app.config['PAGES'], "index.html")
+
+@app.route('/speech', methods=['POST'])
 def upload_file():
     def allowed_file(filename: str):
         return '.' in filename and filename.rsplit('.', 1)[1].lower() == "ogg"
-    if request.method == 'POST':
-        if 'booze' not in request.files:
-            return jsonify({"error":"poshel nahui"}), 501
-        file = request.files['booze']
-        if file.filename == '':
-            return jsonify({"error":"poshel v pizdu"}), 502
-        if file and allowed_file(file.filename):
-            input_path = os.path.join(app.config['UPLOAD_FOLDER'], "speech_input.webm")
-            out_path = os.path.join(app.config['UPLOAD_FOLDER'], "speech.ogg")
-            file.save(input_path)
-            # os.system(f'mkvextract {filepath} tracks --raw 0:speech.ogg')
-            requests.post('http://converter:4000/ffmpeg', json={'args': ['-i', input_path, '-c:a', 'libopus', out_path]})
-            txt = cmspch()
-            return jsonify({"result":txt})
-        return jsonify({"error":"hacker"}), 503
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form method=post enctype=multipart/form-data>
-      <input type=file name=booze>
-      <input type=submit value=Upload>
-    </form>
-    '''
+    if 'booze' not in request.files:
+        return jsonify({"error":"poshel nahui"}), 501
+    file = request.files['booze']
+    if file.filename == '':
+        return jsonify({"error":"poshel v pizdu"}), 502
+    if file and allowed_file(file.filename):
+        input_path = os.path.join(app.config['UPLOAD_FOLDER'], "speech_input.webm")
+        out_path = os.path.join(app.config['UPLOAD_FOLDER'], "speech.ogg")
+        file.save(input_path)
+        # os.system(f'mkvextract {filepath} tracks --raw 0:speech.ogg')
+        requests.post('http://converter:4000/ffmpeg', json={'args': ['-y', '-i', input_path, '-c:a', 'libopus', out_path]})
+        txt = cmspch()
+        return jsonify({"result":txt})
+    return jsonify({"error":"hacker"}), 503
+
+@app.route('/_next/<path:next>', methods=['GET'])
+def pizdaebanaja(next):
+    print(next)
+    return send_from_directory(app.config['PAGES'], '_next/' + next)
+    # return '''
+    # <!doctype html>
+    # <title>Upload new File</title>
+    # <h1>Upload new File</h1>
+    # <form method=post enctype=multipart/form-data>
+    #   <input type=file name=booze>
+    #   <input type=submit value=Upload>
+    # </form>
+    # '''
