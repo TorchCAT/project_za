5f5d32a273f5e9155424921ac42a3e9d69892dc9
TorchCAT <Admin@localhost>
2022-02-05 17:18:05 +0000

    requests counter

--- docker_cumpot.sh
+++
@@ -1,2 +0,0 @@
-cd ~/testass
-/home/dvoell/.local/bin/docker-compose up -d --build --force-recreate
--- main.py
+++ main.py
@@ -5,33 +5,48 @@ import requests
 
 UPLOAD_FOLDER = 'uploads'
 
+requests_speech = 0
+requests_main = 0
+requests_speech_success = 0
+
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['JSON_AS_ASCII'] = False
 app.config['PAGES'] = 'pages'
 
+
 @app.route('/', methods=["GET"])
 def index():
+    global requests_main
+    requests_main = requests_main+1
     return send_from_directory(app.config['PAGES'], "index.html")
+
 
 @app.route('/speech', methods=['POST'])
 def upload_file():
+    global requests_speech, requests_speech_success
+    requests_speech = requests_speech+1
+
     def allowed_file(filename: str):
         return '.' in filename and filename.rsplit('.', 1)[1].lower() == "ogg"
     if 'booze' not in request.files:
-        return jsonify({"error":"poshel nahui"}), 501
+        return jsonify({"error": "poshel nahui"}), 501
     file = request.files['booze']
     if file.filename == '':
-        return jsonify({"error":"poshel v pizdu"}), 502
+        return jsonify({"error": "poshel v pizdu"}), 502
     if file and allowed_file(file.filename):
-        input_path = os.path.join(app.config['UPLOAD_FOLDER'], "speech_input.webm")
+        input_path = os.path.join(
+            app.config['UPLOAD_FOLDER'], "speech_input.webm")
         out_path = os.path.join(app.config['UPLOAD_FOLDER'], "speech.ogg")
         file.save(input_path)
         # os.system(f'mkvextract {filepath} tracks --raw 0:speech.ogg')
-        requests.post('http://converter:4000/ffmpeg', json={'args': ['-y', '-i', input_path, '-c:a', 'libopus', out_path]})
+        requests.post('http://converter:4000/ffmpeg',
+                      json={'args': ['-y', '-i', input_path, '-c:a', 'libopus', out_path]})
         txt = cmspch()
+        requests_speech_success += 1
         return jsonify(txt)
-    return jsonify({"error":"hacker"}), 503
+    return jsonify({"error": "hacker"}), 503
+
 
 @app.route('/_next/<path:next>', methods=['GET'])
 def pizdaebanaja(next):
@@ -46,6 +61,8 @@ def pizdaebanaja(next):
     #   <input type=submit value=Upload>
     # </form>
     # '''
-@app.route('/auetest', methods=["GET"])
-def auetest():
-    return jsonify({"success":"test"}), 200
+
+
+@app.route('/metrics', methods=["GET"])
+def metrics():
+    return jsonify({"requests_main": requests_main, "requests_speech": requests_speech, "requests_speech_success": requests_speech_success}), 200
